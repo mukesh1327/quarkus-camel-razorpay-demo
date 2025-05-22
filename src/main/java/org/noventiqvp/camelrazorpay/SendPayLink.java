6635b14f70917db28eb1a3fa1eba1b3bd9f0edbc
mukesh1327 <mugicoder13@gmail.com>
2025-05-22 07:39:10 +0000

    Updated send payment link

--- src/main/java/org/noventiqvp/camelrazorpay/SendPayLink.java
+++ src/main/java/org/noventiqvp/camelrazorpay/SendPayLink.java
@@ -31,65 +31,14 @@ public class SendPayLink extends RouteBuilder {
             .setHeader("Authorization", constant(authHeader)) // Injected from config
             .removeHeaders("CamelHttp*")
             .to("https://api.razorpay.com/v1/payment_links?bridgeEndpoint=true")
-            .log("Razorpay create response: ${body}")
-            .unmarshal().json()
-
-            .setHeader("payment_id", simple("${body[id]}"))
-            .setHeader("reference_id", simple("${body[reference_id]}"))
-
-            .setBody(constant((Object) null))
-            .removeHeaders("CamelHttp*")
-            .setHeader("Authorization", constant(authHeader))
-            .setHeader("CamelHttpMethod", constant("GET"))
-            .toD("https://api.razorpay.com/v1/payment_links/${header.payment_id}?bridgeEndpoint=true")
             .log("Razorpay get response: ${body}")
             .unmarshal().json()
 
             .setBody().simple(
-                "{\"reference_id\": \"${header.reference_id}\", " +
-                "\"payment_id\": \"${header.payment_id}\", " +
+                "{\"reference_id\": \"${body[reference_id]}\", " +
+                "\"plink_id\": \"${body[id]}\", " +
                 "\"short_url\": \"${body[short_url]}\"}"
             );
     }
 
 }
-
-
-// public class SendPayLink extends RouteBuilder {
-
-//     @Override
-//     public void configure() throws Exception {
-
-//         // Unified POST: Create payment link and fetch short_url
-//         from("platform-http:/create-payment-link?httpMethodRestrict=POST")
-//         .routeId("create-and-fetch-short-url")
-//         .log("Received payment creation request: ${body}")
-//         .setHeader("Content-Type", constant("application/json"))
-//         .setHeader("Authorization", constant("Basic cnpwX3Rlc3RfbTdERXBkeE01YUNHWks6dk1oVWlGelUxNHVCSmVGRFl6Y09OOVJHCg=="))
-//         .removeHeaders("CamelHttp*")
-//         .to("https://api.razorpay.com/v1/payment_links?bridgeEndpoint=true")
-//         .log("Razorpay create response: ${body}")
-//         .unmarshal().json()
-    
-//         // Store needed fields
-//         .setHeader("payment_id", simple("${body[id]}"))
-//         .setHeader("reference_id", simple("${body[reference_id]}"))
-    
-//         // Fix: Reset body before GET request
-//         .setBody(constant((Object) null))
-//         .removeHeaders("CamelHttp*") // Optional again
-//         .setHeader("Authorization", constant("Basic cnpwX3Rlc3RfbTdERXBkeE01YUNHWks6dk1oVWlGelUxNHVCSmVGRFl6Y09OOVJHCg=="))
-//         .setHeader("CamelHttpMethod", constant("GET"))
-    
-//         .toD("https://api.razorpay.com/v1/payment_links/${header.payment_id}?bridgeEndpoint=true")  // or use .recipientList(simple("https://api.razorpay.com/v1/payment_links/${header.payment_id}?bridgeEndpoint=true"))
-//         .log("Razorpay get response: ${body}")
-//         .unmarshal().json()
-    
-//         .setBody().simple(
-//             "{\"reference_id\": \"${header.reference_id}\", " +
-//             "\"payment_id\": \"${header.payment_id}\", " +
-//             "\"short_url\": \"${body[short_url]}\"}"
-//         );
-    
-//     }
-// }
